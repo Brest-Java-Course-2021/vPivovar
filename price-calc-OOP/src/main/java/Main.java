232fb292bf334b6108be98e458dd333a7c5e8df3
PanVaukan <vladimirpivovar@gmail.com>
2021-05-20 08:52:34 +0300

    A small code fix was made. The exception handling logic is questionable. The execution script can still only perform one calculation.

--- price-calc-OOP/src/main/java/Main.java
+++ price-calc-OOP/src/main/java/Main.java
@@ -5,17 +5,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    public static void main(String[] args) throws RequestFailureException {
+    public static void main(String[] args) /* throws RequestFailureException */ {
         new Main().run();
     }
 
-    public void run() throws RequestFailureException {
+    public void run() /* throws RequestFailureException */ {
 
         var appContext = new ClassPathXmlApplicationContext("spring-config.xml");
 
         try {
 
-            appContext.getBean(DeliveryPriceCalculatorFactory.class).create().request();
+//          appContext.getBean(DeliveryPriceCalculatorFactory.class).create().request();
+
+            new ClassPathXmlApplicationContext("spring-config.xml")
+                    .getBean(DeliveryPriceCalculatorFactory.class)
+                    .create()
+                    .request();
 
         } catch (RequestInterruptedException | RequestFailureException e) {
             System.out.print("bye");
