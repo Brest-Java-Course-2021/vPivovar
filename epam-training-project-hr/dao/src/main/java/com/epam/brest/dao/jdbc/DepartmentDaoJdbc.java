f211fe86a4eeacd04517f1946dcebbd43a152f99
PanVaukan <vladimirpivovar@gmail.com>
2021-06-07 00:46:39 +0300

    A JDBC-template was injected into the DAO-class via xml-config files.

--- epam-training-project-hr/dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbc.java
+++ epam-training-project-hr/dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbc.java
@@ -2,13 +2,47 @@ package com.epam.brest.dao.jdbc;
 
 import com.epam.brest.dao.DepartmentDao;
 import com.epam.brest.model.Department;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DepartmentDaoJdbc implements DepartmentDao {
 
+    private static final String SQL_GET_ALL_DEPARTMENTS =
+            "SELECT D.DEPARTMENT_ID, D.DEPARTMENT_NAME FROM DEPARTMENT AS D ORDER BY D.DEPARTMENT_NAME";
+
+    // Allows you to create dynamic parameterized queries:
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    /*
+    public DepartmentDaoJdbc(DataSource dataSource) {
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+    */
+
+    public DepartmentDaoJdbc(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public List<Department> findAll() {
-        return null;
+        return namedParameterJdbcTemplate.query(SQL_GET_ALL_DEPARTMENTS, new DepartmentRowMapper());
     }
+
+
+    private class DepartmentRowMapper implements RowMapper<Department> {
+
+        @Override
+        public Department mapRow(ResultSet resultSet, int i) throws SQLException {
+            Department department = new Department();
+            department.setDepartmentId(resultSet.getInt("DEPARTMENT_ID"));
+            department.setDepartmentName(resultSet.getString("DEPARTMENT_NAME"));
+            return department;
+        }
+    }
+
 }
--- epam-training-project-hr/dao/src/test/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbcTest.java
+++ epam-training-project-hr/dao/src/test/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbcTest.java
@@ -1,16 +1,29 @@
 package com.epam.brest.dao.jdbc;
 
 import com.epam.brest.dao.DepartmentDao;
+import com.epam.brest.model.Department;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:test-dao.xml", "classpath*:test-db.xml"})
 public class DepartmentDaoJdbcTest {
 
+    @Autowired
     private DepartmentDao departmentDao;
 
     @Test
     public void findAllTest() {
+
+        List<Department> departments = departmentDao.findAll();
+        Assert.assertNotNull(departments);
+        Assert.assertTrue(departments.size() > 0);
 
     }
 }
